2351bb4b4e646cb58259af078473ae86e25c2565
Yohann Loiseau <yohann.loiseau@etu.univ-orleans.fr>
2024-03-26 16:46:31 +0100

    Début page d'accueil

--- lib/ui/accueil.dart
+++ lib/ui/accueil.dart
@@ -1,89 +1,56 @@
 import 'package:flutter/material.dart';
 
-class MyAccueil extends StatefulWidget {
-  const MyAccueil({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Accueil extends StatefulWidget {
+  const Accueil({super.key});
 
   @override
-  State<MyAccueil> createState() => _MyAccueilPageState();
+  State<Accueil> createState() => _AccueilPageState();
 }
 
-class _MyAccueilPageState extends State<MyAccueil> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _AccueilPageState extends State<Accueil> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xFFAF8DE7),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+        title: const Text('Le nombre magique', style: TextStyle(color: Colors.deepPurple),),
+        backgroundColor: const Color(0xFFAF8DE7),),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.all(80.0),),
+              ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 40, vertical: 20)), ,
+                    backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  ),
+                  child: const Text('Jouer', style: TextStyle(color: Color(0xFFD2CCEF)),),
+                  onPressed: () {  },//onPressed: () => context.go('mettre le chemin'),
+              ),
+              const Padding(padding: EdgeInsets.all(20.0),),
+              ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 20)),
+                    backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  ),
+                  child: const Text('Voir mes scores', style: TextStyle(color: Color(0xFFD2CCEF)),),
+                  onPressed: () {  },//onPressed: () => context.go('mettre le chemin'),
+                //onPressed: () => context.go(''),
+              ),
+              const Padding(padding: EdgeInsets.all(20.0),),
+              ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 20)),
+                    backgroundColor: MaterialStatePropertyAll(Colors.deepPurple),
+                  ),
+                  child: const Text('Règles du jeu', style: TextStyle(color: Color(0xFFD2CCEF)),),
+                  onPressed: () {  },//onPressed: () => context.go('mettre le chemin'),
+                //onPressed: () => context.go(''),
+              ),
+            ],
+          )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
